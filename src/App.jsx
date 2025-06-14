f060ede1ecc80e32fae66ff6bc365e144b2ae552
Safayet <mdsafayetsorder.cse@gmail.com>
2025-06-15 02:13:02 +0600

    structure added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className='flex justify-center gap-8 mt-2 border py-3 bg-teal-400 text-white '>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Services</a>
+        <a href="">Management</a>
+        <a href="">Contact Us</a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
